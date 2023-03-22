1c6346b11b565008034cd5a573d25ca7b769b046
Iliya <ilyakuzmin9@gmail.com>
2023-03-22 18:10:59 +0300

    add: commit_1 in feature_branch_002

--- xmlconvertor.py
+++ xmlconvertor.py
@@ -50,3 +50,5 @@ print("this is commit in feature_branch_2")
 print("this is commit in feature_branch_001")
 
 print("this is commit_2 in feature_branch_001")
+
+print("this is commit_1 in feature_branch_002")
